b5babba1cb7d3e0d881f3008b3bc4182b655a392
David Schneider <mail@schnav.id>
2022-06-05 23:30:30 +0200

    smol entity

--- src/main/kotlin/org/luovuutesi/api/Entity.kt
+++ src/main/kotlin/org/luovuutesi/api/Entity.kt
@@ -2,8 +2,4 @@ package org.luovuutesi.api
 
 interface Entity {
     val id: Long
-
-    fun <T : Component> addComponent(component: T)
-    fun <T : Component> getComponent(componentClass: Class<T>): T?
-    fun <T : Component> removeComponent(componentClass: Class<T>)
 }
