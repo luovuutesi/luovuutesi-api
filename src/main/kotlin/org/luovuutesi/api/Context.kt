9a7229362d4d1eb7c0e0479a2cead5546a92098f
David Schneider <mail@schnav.id>
2022-06-05 02:28:07 +0200

    ecs systems

--- src/main/kotlin/org/luovuutesi/api/Context.kt
+++ src/main/kotlin/org/luovuutesi/api/Context.kt
@@ -3,4 +3,6 @@ package org.luovuutesi.api
 interface Context {
     fun createEntity(): Entity
     fun destroyEntity(entity: Entity)
+
+    fun register(system: System)
 }
---
+++ src/main/kotlin/org/luovuutesi/api/System.kt
@@ -0,0 +1,18 @@
+package org.luovuutesi.api
+
+interface System {
+    val requiredComponents: Collection<Component>
+        get() = setOf()
+
+    fun beforeStart(context: Context, entities: Collection<Entity>) {}
+    fun start(context: Context, entities: Collection<Entity>) {}
+    fun afterStart(context: Context, entities: Collection<Entity>) {}
+
+    fun beforeUpdate(context: Context, entities: Collection<Entity>) {}
+    fun startUpdate(context: Context, entities: Collection<Entity>) {}
+    fun afterUpdate(context: Context, entities: Collection<Entity>) {}
+
+    fun beforeStop(context: Context, entities: Collection<Entity>) {}
+    fun stop(context: Context, entities: Collection<Entity>) {}
+    fun afterStop(context: Context, entities: Collection<Entity>) {}
+}
